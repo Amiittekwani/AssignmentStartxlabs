8f0c1daa43578d772e7a53bf19939443fc85f787
Amiittekwani <82334828+Amiittekwani@users.noreply.github.com>
2022-01-07 22:40:36 +0530

    Update TimeSeconds.js

--- TimeSeconds.js
+++ TimeSeconds.js
@@ -6,11 +6,3 @@ let minutes = Math.floor((T%3600)/60);
 let seconds = ((T%3600)%60);
 
 console.log(`hours = ${hours}, minutes = ${minutes} and seconds = ${seconds}`)
-// let seconds = T%60;
-// console.log(minute);
-// console.log(seconds);
-// let hours = Math.floor(T/3600);
-// console.log(hours)
-// if (hours >= 1){
-//     minute = minute%hours
-// }
